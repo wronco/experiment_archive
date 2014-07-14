511b3811f493fe5f017b2ce6ab1ee71f8a97deec
Prelang Builder <builder@prelang.com>
2014-07-14 22:49:25 +0000

    Defined validation for attribute 'screenshot_variation' on model 'Experiment'

--- app/models/experiment.rb
+++ app/models/experiment.rb
@@ -1,4 +1,5 @@
 class Experiment < ActiveRecord::Base
   belongs_to :user
   validates_formatting_of :screenshot_original, using: :url
+  validates_formatting_of :screenshot_variation, using: :url
 end
