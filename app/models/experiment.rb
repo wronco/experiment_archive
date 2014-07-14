276723e83602efaf5200b45994b9520e934cbc6a
Prelang Builder <builder@prelang.com>
2014-07-14 22:49:25 +0000

    Defined validation for attribute 'experiment_url' on model 'Experiment'

--- app/models/experiment.rb
+++ app/models/experiment.rb
@@ -2,4 +2,5 @@ class Experiment < ActiveRecord::Base
   belongs_to :user
   validates_formatting_of :screenshot_original, using: :url
   validates_formatting_of :screenshot_variation, using: :url
+  validates_formatting_of :experiment_url, using: :url
 end
