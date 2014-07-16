e46810615ec86a32ca8a810bd4540ff8f18bc044
Will Ronco <william.ronco@gmail.com>
2014-07-16 11:08:19 -0700

    Expainerater

--- app/controllers/experiments_controller.rb
+++ app/controllers/experiments_controller.rb
@@ -11,9 +11,24 @@ class ExperimentsController < ApplicationController
     @experiment = current_user.experiments.find(params[:id])
   end
 
+  def update
+    @experiment = current_user.experiments.find(params[:id])
+    if @experiment.update_attributes(permitted_params(params[:experiment]))
+      redirect_to edit_experiment_path(@experiment), notice: "Saved."
+    else
+      render action: :edit    
+    end
+  end
+
   def refresh
+    current_user.update_attributes(optimizely_refresh_started_at: Time.now)
     current_user.delay.refresh_from_optimizely
     redirect_to experiments_path, notice: "Optimizely refreshing now. Reload this page in a couple of minutes to see the results."
   end
 
+  private
+  def permitted_params(params)
+    params.permit(:winner, :confidence, :brief, :hypothesis)
+  end
 end
+
